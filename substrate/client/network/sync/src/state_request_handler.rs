f080ddf11b7234a39a15dd3ff88ff1b1e75e26fc
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2024-08-16 15:27:59 +0800

    Compress state response message and remove block body request attribute for LightState sync

--- substrate/client/network/sync/src/state_request_handler.rs
+++ substrate/client/network/sync/src/state_request_handler.rs
@@ -265,7 +265,11 @@ where
 
 			let mut data = Vec::with_capacity(response.encoded_len());
 			response.encode(&mut data)?;
-			Ok(data)
+
+			let compressed_response = zstd::stream::encode_all(data.as_slice(), 0)
+				.map_err(HandleRequestError::Compress)?;
+
+			Ok(compressed_response)
 		} else {
 			Err(())
 		};
@@ -292,4 +296,7 @@ enum HandleRequestError {
 
 	#[error("Failed to send response.")]
 	SendResponse,
+
+	#[error("Failed to compress response: {0}.")]
+	Compress(std::io::Error),
 }
